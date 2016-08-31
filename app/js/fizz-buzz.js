317413b9868b1309ee6a7c0436aa4212b1bf8590
Khaled HALLEB <khalleb@Khaleds-MacBook-Pro.local>
2016-08-31 15:32:30 +0200

    buzz test case red step

--- app/js/fizz-buzz.js
+++ app/js/fizz-buzz.js
@@ -1,1 +1,11 @@
-var FizzBuzz;
+var FizzBuzz={
+
+    proceed: function (inputNumber) {
+        if(inputNumber%3==0){
+            return "fizz";
+        }
+        return inputNumber;
+    }
+    
+    
+};
--- test/fizz-buzz-test.js
+++ test/fizz-buzz-test.js
@@ -1,7 +1,15 @@
 QUnit.test('when input number is divisible by three then display fizz', function (assert) {
-    assert.strictEqual(FizzBuzz.proceed(3), 'fuzz', 'number divisible by three');
-    assert.strictEqual(FizzBuzz.proceed(9), 'fuzz', 'number divisible by three');
-    assert.strictEqual(FizzBuzz.proceed(12), 'fuzz', 'number divisible by three');
-    assert.strictEqual(FizzBuzz.proceed(18), 'fuzz', 'number divisible by three');
+    assert.strictEqual(FizzBuzz.proceed(3), 'fizz', '3 divisible by three');
+    assert.strictEqual(FizzBuzz.proceed(9), 'fizz', '9 divisible by three');
+    assert.strictEqual(FizzBuzz.proceed(12), 'fizz', '12 divisible by three');
+    assert.strictEqual(FizzBuzz.proceed(18), 'fizz', '18 divisible by three');
+
+});
+
+QUnit.test('when input number is divisible by five then display buzz', function (assert) {
+    assert.strictEqual(FizzBuzz.proceed(5), 'buzz', '5 divisible by three');
+    assert.strictEqual(FizzBuzz.proceed(10), 'buzz', '10 divisible by three');
+    assert.strictEqual(FizzBuzz.proceed(20), 'buzz', '20 divisible by three');
+    assert.strictEqual(FizzBuzz.proceed(25), 'buzz', '25 divisible by three');
 
 });
