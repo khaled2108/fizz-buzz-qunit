a8ed070f198c7b485868be862fc2ed024679527a
Khaled HALLEB <khalleb@Khaleds-MacBook-Pro.local>
2016-08-31 15:48:17 +0200

    fizzbuzz test case : green then refoactor step

--- app/js/fizz-buzz.js
+++ app/js/fizz-buzz.js
@@ -1,14 +1,26 @@
 var FizzBuzz = {
 
     proceed: function (inputNumber) {
-
-        if (inputNumber % 3 == 0) {
+        if (this.isDivisibleByThree(inputNumber) && this.isDivisibleByFive(inputNumber)) {
+            return "fizzbuzz";
+        }
+        else if (this.isDivisibleByThree(inputNumber)) {
             return "fizz";
-        } else if (inputNumber % 5 == 0) {
+        } else if (this.isDivisibleByFive(inputNumber)) {
             return "buzz";
         }
         return inputNumber;
+    },
+    isDivisibleByThree: function (number) {
+        if (number % 3 == 0) {
+            return true;
+        }
+        return false;
+    },
+    isDivisibleByFive: function (number) {
+        if (number % 5 == 0) {
+            return true;
+        }
+        return false;
     }
-
-
 };
--- test/fizz-buzz-test.js
+++ test/fizz-buzz-test.js
@@ -7,9 +7,17 @@ QUnit.test('when input number is divisible by three then display fizz', function
 });
 
 QUnit.test('when input number is divisible by five then display buzz', function (assert) {
-    assert.strictEqual(FizzBuzz.proceed(5), 'buzz', '5 divisible by three');
-    assert.strictEqual(FizzBuzz.proceed(10), 'buzz', '10 divisible by three');
-    assert.strictEqual(FizzBuzz.proceed(20), 'buzz', '20 divisible by three');
-    assert.strictEqual(FizzBuzz.proceed(25), 'buzz', '25 divisible by three');
+    assert.strictEqual(FizzBuzz.proceed(5), 'buzz', '5 divisible by five');
+    assert.strictEqual(FizzBuzz.proceed(10), 'buzz', '10 divisible by five');
+    assert.strictEqual(FizzBuzz.proceed(20), 'buzz', '20 divisible by five');
+    assert.strictEqual(FizzBuzz.proceed(25), 'buzz', '25 divisible by five');
+
+});
+
+QUnit.test('when input number is divisible by five and three then display fizzbuzz', function (assert) {
+    assert.strictEqual(FizzBuzz.proceed(15), 'fizzbuzz', '15 divisible by three and five');
+    assert.strictEqual(FizzBuzz.proceed(30), 'fizzbuzz', '10 divisible by three and five');
+    assert.strictEqual(FizzBuzz.proceed(45), 'fizzbuzz', '20 divisible by three and five');
+    assert.strictEqual(FizzBuzz.proceed(60), 'fizzbuzz', '25 divisible by three and five');
 
 });
