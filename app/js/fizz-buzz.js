e58d13aab0a0ceeebdbe6c71780c6e52b9da890c
Khaled HALLEB <khalleb@Khaleds-MacBook-Pro.local>
2016-08-31 15:34:35 +0200

    buzz test case : refactor then green step

--- app/js/fizz-buzz.js
+++ app/js/fizz-buzz.js
@@ -3,6 +3,8 @@ var FizzBuzz={
     proceed: function (inputNumber) {
         if(inputNumber%3==0){
             return "fizz";
+        }else if(inputNumber%5==0){
+            return "buzz";
         }
         return inputNumber;
     }
